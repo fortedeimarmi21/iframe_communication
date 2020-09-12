d475dfb944dfdaad5119d27c8fda5c0b80c221cc
lolka <lolka@mail.ru>
2020-09-12 23:05:33 +0200

    add setter for communication window;

--- src/communication.ts
+++ src/communication.ts
@@ -1,33 +1,41 @@
-import { EventDispatcher } from './dispatcher';
+import { EventDispatcher } from "./dispatcher";
 
 class CommunicationManager extends EventDispatcher {
   constructor(private _origin: string, private _source: any) {
     super();
   }
 
+  set targetWindow(target: any) {
+    this._source = target;
+  }
+
   public listen(): void {
-    window.addEventListener('message', this.handleMessageEvent);
+    window.addEventListener("message", this.handleMessageEvent);
   }
 
   public sendMessage = (message: any): void => {
     this._source.postMessage(message, this._origin);
-  }
-  
+  };
+
   public onMessage(event: MessageEvent) {
-    console.debug('CommunicationManager:: handleMessageEvent', event, this);
+    console.debug("CommunicationManager:: handleMessageEvent", event, this);
   }
 
   private handleMessageEvent = (event: MessageEvent) => {
-    console.debug('CommunicationManager:: handleMessageEvent', event, this);
+    console.debug("CommunicationManager:: handleMessageEvent", event, this);
 
     if (this._origin !== event.origin) {
-      console.debug('CommunicationManager:: message from unauthorized origin', event, this);
+      console.debug(
+        "CommunicationManager:: message from unauthorized origin",
+        event,
+        this
+      );
       return;
     }
     this._source = !this._source && event.source ? event.source : this._source;
 
     this.onMessage(event);
-  }
+  };
 }
 
 export default CommunicationManager;
